c62ddcd928c1866ecafaf79fcb26064009e58059
manjuram-inbox <52387920+manjuram-inbox@users.noreply.github.com>
2023-05-10 14:34:42 -0500

    Update main.tf
    Create a pool and provider with conditions

--- modules/identity-pool/main.tf
+++ modules/identity-pool/main.tf
@@ -1,3 +1,36 @@
-/*************************************************
-  Common module to create an Workload Identity Pool in GCP
-/*************************************************
+/*************************************************************************************************
+  Common module that creates a Workload Identity Pool with one Identity Provider per pool for Terraform Cloud integrarion with GCP
+    
+  For Workload identity tokens from Terraform Cloud refer to https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/workload-identity-tokens 
+  
+/*************************************************************************************************
+      
+resource "google_iam_workload_identity_pool" "identity-pool" {
+  workload_identity_pool_id = var.identity_pool_id
+  display_name              = var.identity_pool_name
+  description               = var.identity_pool_desc
+  disabled                  = var.identity_pool_disabled
+}
+    
+resource "google_iam_workload_identity_pool_provider" "example" {
+  workload_identity_pool_id           = google_iam_workload_identity_pool.identity-pool.workload_identity_pool_id
+  workload_identity_pool_provider_id  = var.pool_provider_id
+  display_name                        = var.pool_provider_name
+  description                         = var.pool_provider_desc
+  disabled                            = var.pool_provider_disabled
+
+  attribute_mapping                   = {
+    "attribute.tfc_organization_id"   = "assertion.terraform_organization_id"
+    "attribute.tfc_project_id"        = "assertion.terraform_project_id"
+    "attribute.tfc_project_name"      = "assertion.terraform_project_name"
+    "google.subject"                  = "assertion.terraform_workspace_id"
+    "attribute.tfc_workspace_name"    = "assertion.terraform_workspace_name"
+    "attribute.tfc_workspace_env"     = "assertion.terraform_workspace_name.split('-')[assertion.terraform_workspace_name.split('-').size() -1 ]'"
+  }
+  oidc {
+    allowed_audiences = var.allowed_audiences
+    issuer_uri        = "https://app.terraform.io"
+  }
+  attribute_condition                =  "attribute.tfc_organization_id == " + var.tfc_organization_id + 
+                                        " && attribute.tfc_workspace_env == " + var.tfc_workspace_env
+}
