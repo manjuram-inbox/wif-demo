61cc42d4bf5ecd6d9d3ef19b3822a1747253b0bf
manjuram-inbox <52387920+manjuram-inbox@users.noreply.github.com>
2023-05-11 14:38:54 -0500

    Create variables.tf

---
+++ modules/identity-pool/variables.tf
@@ -0,0 +1,46 @@
+variable "identity_pool_id" {
+  description = ""
+  type        = string
+}
+
+variable "identity_p0ol_name" {
+  description = ""
+  type        = string
+}
+
+variable "identity_p0ol_desc" {
+  description = ""
+  type        = string
+}
+
+variable "identity_pool_disabled" {
+  description = ""
+  type        = string
+}
+
+variable "pool_provider_id" {
+  description = ""
+  type        = string
+}
+variable "pool_provider_name" {
+  description = ""
+  type        = string
+}
+variable "pool_provider_desc" {
+  description = ""
+  type        = string
+}
+variable "pool_provider_disabled" {
+  description = ""
+  type        = string
+}
+
+
+variable "tfc_organization_id" {
+  description = ""
+  type        = string
+}
+variable "tfc_workspace_env" {
+  description = ""
+  type        = string
+}
